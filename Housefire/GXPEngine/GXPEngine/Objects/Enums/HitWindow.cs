dcba970b8added0e7db70efb43cb3d9006e9f6dc
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-11 17:23:32 +0100

    This class determines the timing of notes you can hit

---
+++ Housefire/GXPEngine/GXPEngine/Objects/Enums/HitWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Objects.Enums
+{
+    public static class HitWindow
+    {
+        public static int miss = 120;
+        public static int just = 100;
+        public static int okay = 60;
+        public static int good = 40;
+        public static int perfect = 20;
+    }
+}
