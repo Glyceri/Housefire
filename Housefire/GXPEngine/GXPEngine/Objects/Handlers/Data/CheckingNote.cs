f4f052f77458deff20c829b978fbebb33bab75af
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-11 17:24:38 +0100

    Note object that will be checked.

---
+++ Housefire/GXPEngine/GXPEngine/Objects/Handlers/Data/CheckingNote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Objects.Handlers.Data
+{
+    public struct CheckingNote
+    {
+
+        public int noteTime;
+        public bool shouldRewardPoints;
+        public CheckingNote(int note, bool shouldRewardPoints)
+        {
+            noteTime = note;
+            this.shouldRewardPoints = shouldRewardPoints;
+        }
+    }
+}
