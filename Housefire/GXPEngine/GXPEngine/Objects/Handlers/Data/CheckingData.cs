e67511e9339b6d7527af311b3839982d9929e08a
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-11 17:24:25 +0100

    A very crude way of checking if the time you press the keyboard matches the time the notes should be hit.

---
+++ Housefire/GXPEngine/GXPEngine/Objects/Handlers/Data/CheckingData.cs
@@ -0,0 +1,52 @@
+﻿using GXPEngine.Objects.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Objects.Handlers.Data
+{
+    public class CheckingData
+    {
+        public bool noteHit;
+        public PrecisionLevel precisionLevel;
+
+        public CheckingData(int noteTime, int hitTime, BeatmapHandler beatmapHandler)
+        {
+            hitTime -= (int)(beatmapHandler.BPM_calc / 4) - (int)(beatmapHandler.activeBeatmap.offset/4);
+            noteTime += BeatmapHandler.beatOffset;
+            precisionLevel = PrecisionLevel.UltraMiss;
+            noteHit = HasHit(noteTime, hitTime);
+        }
+
+        bool HasHit(int noteTime, int hitTime)
+        {
+            if (noteTime + HitWindow.miss > hitTime && noteTime - HitWindow.miss < hitTime)
+            {
+                precisionLevel = PrecisionLevel.Miss;
+                if (noteTime + HitWindow.just > hitTime && noteTime - HitWindow.just < hitTime)
+                {
+                    precisionLevel = PrecisionLevel.Just;
+                    if (noteTime + HitWindow.okay > hitTime && noteTime - HitWindow.okay < hitTime)
+                    {
+                        precisionLevel = PrecisionLevel.Okay;
+                        if (noteTime + HitWindow.good > hitTime && noteTime - HitWindow.good < hitTime)
+                        {
+                            precisionLevel = PrecisionLevel.Good;
+                            if (noteTime + HitWindow.perfect > hitTime && noteTime - HitWindow.perfect < hitTime)
+                            {
+                                precisionLevel = PrecisionLevel.Perfect;
+                            }
+                            return true;
+                        }
+                        return true;
+                    }
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
