411fe7d14e3633e96847c80ea14f1f8b2a7f5c34
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-14 10:55:33 +0100

    Thread that plays audio

---
+++ Housefire/GXPEngine/GXPEngine/Core/Audio/AudioThread.cs
@@ -0,0 +1,26 @@
+﻿using NAudio.Wave;
+using System;
+using System.Threading;
+
+namespace GXPEngine.Core.Audio
+{
+    public class AudioThread
+    {
+        public static void PlayAudio(object audioObject)
+        {
+            try
+            {
+                (audioObject as AudioObject).audioPlayer?.Play();
+                (audioObject as AudioObject).audioFile.Volume = AudioHandler.volume / (float)100;
+                while ((audioObject as AudioObject).audioPlayer?.PlaybackState != PlaybackState.Stopped && !(audioObject as AudioObject).disposed)
+                {
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            (audioObject as AudioObject).Dispose();
+        }
+    }
+}
