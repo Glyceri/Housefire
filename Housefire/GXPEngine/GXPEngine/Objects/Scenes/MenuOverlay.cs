51ce8f9bc91df3354cfbbd588fb1c9e262d7c533
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-16 15:57:55 +0100

    Overlay menu

---
+++ Housefire/GXPEngine/GXPEngine/Objects/Scenes/MenuOverlay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Objects.Scenes
+{
+    public class MenuOverlay : BeatmapButtonEasyDraw
+    {
+
+        public MenuOverlay() : base(1920, 1080, false)
+        {
+            using (Bitmap bitmap = new Bitmap("backgroundpanel.png"))
+            {
+                DrawSprite(bitmap, new Vector2(1920 / (float)bitmap.Width, 1080 / (float)bitmap.Height), 0, 0, false);
+            }
+        }
+    }
+}
