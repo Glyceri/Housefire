190ed674c40e60f39e46aec79f152c72ba7a34ce
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-16 15:53:45 +0100

    Highscore object

---
+++ Housefire/GXPEngine/GXPEngine/Objects/Handlers/Data/Highscore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Objects.Handlers.Data
+{
+    public class Highscore
+    {
+        public string name;
+        public int score;
+        public int combo;
+
+        public Highscore(string name, int score, int combo)
+        {
+            this.name = name;
+            this.score = score;
+            this.combo = combo;
+        }
+
+    }
+}
