45bf97f558bad3561c7765379859b98a747a8618
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-18 18:29:48 +0100

    Updated keybinds

--- Housefire/GXPEngine/GXPEngine/Objects/Player.cs
+++ Housefire/GXPEngine/GXPEngine/Objects/Player.cs
@@ -21,13 +21,13 @@ namespace GXPEngine.Objects
             if(playerNum == 1)
             {
                 name = "Player 1";
-                keybinds = new List<VKeys[]>() { new VKeys[1] { VKeys.SPACE }, new VKeys[2] { VKeys.KEY_F, VKeys.KEY_J }, new VKeys[3] { VKeys.KEY_F, VKeys.SPACE, VKeys.KEY_J }, new VKeys[4] { VKeys.KEY_D, VKeys.KEY_F, VKeys.KEY_J, VKeys.KEY_K }, new VKeys[5] { VKeys.KEY_D, VKeys.KEY_F, VKeys.SPACE, VKeys.KEY_J, VKeys.KEY_K }, new VKeys[6] { VKeys.KEY_S, VKeys.KEY_D, VKeys.KEY_F, VKeys.KEY_J, VKeys.KEY_K, VKeys.KEY_L }, new VKeys[7] { VKeys.KEY_S, VKeys.KEY_D, VKeys.KEY_F, VKeys.SPACE, VKeys.KEY_J, VKeys.KEY_K, VKeys.KEY_L } };
+                keybinds = new List<VKeys[]>() { new VKeys[1] { VKeys.SPACE }, new VKeys[2] { VKeys.KEY_S, VKeys.KEY_F }, new VKeys[3] { VKeys.KEY_S, VKeys.SPACE, VKeys.KEY_F }, new VKeys[4] { VKeys.KEY_A, VKeys.KEY_S, VKeys.KEY_F, VKeys.KEY_G }, new VKeys[5] { VKeys.KEY_D, VKeys.KEY_F, VKeys.SPACE, VKeys.KEY_J, VKeys.KEY_K } };
             }
 
             if (playerNum == 2)
             {
                 name = "Player 2";
-                keybinds = new List<VKeys[]>() { new VKeys[1] { VKeys.UP }, new VKeys[2] { VKeys.LEFT, VKeys.RIGHT }, new VKeys[3] { VKeys.LEFT, VKeys.UP, VKeys.RIGHT }, new VKeys[4] { VKeys.LEFT, VKeys.UP, VKeys.DOWN, VKeys.RIGHT }, new VKeys[5] { VKeys.LEFT, VKeys.UP, VKeys.RCONTROL, VKeys.DOWN, VKeys.RIGHT }, new VKeys[6] { VKeys.RCONTROL, VKeys.LEFT, VKeys.UP, VKeys.DOWN, VKeys.RIGHT, VKeys.NUMPAD0 }, new VKeys[7] { VKeys.RCONTROL, VKeys.LEFT, VKeys.UP, VKeys.RSHIFT, VKeys.DOWN, VKeys.RIGHT, VKeys.NUMPAD0 } };
+                keybinds = new List<VKeys[]>() { new VKeys[1] { VKeys.RMENU }, new VKeys[2] { VKeys.KEY_J, VKeys.KEY_L }, new VKeys[3] { VKeys.KEY_J, VKeys.RMENU, VKeys.KEY_L }, new VKeys[4] { VKeys.KEY_H, VKeys.KEY_J, VKeys.KEY_L, VKeys.OEM_1 }, new VKeys[5] { VKeys.KEY_H, VKeys.KEY_J, VKeys.RMENU, VKeys.KEY_L, VKeys.OEM_1 } };
             }
         }
 
