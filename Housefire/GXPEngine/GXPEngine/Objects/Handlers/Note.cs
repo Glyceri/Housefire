e7e1af2c64f3cd85281062f7ebcb00233888f97f
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-18 18:26:53 +0100

    Now holds playsound

--- Housefire/GXPEngine/GXPEngine/Objects/Handlers/Note.cs
+++ Housefire/GXPEngine/GXPEngine/Objects/Handlers/Note.cs
@@ -13,16 +13,18 @@ namespace GXPEngine.Objects.Handlers
         public int length;
         public int lane;
 
+        public string hitSound = "";
 
         
 
         public Note() { }
 
-        public Note(int lane, int hitTime, int length = 1)
+        public Note(int lane, int hitTime, string hitSound, int length = 1)
         {
             this.hitTime = hitTime;
             this.length = length;
             this.lane = lane;
+            this.hitSound = hitSound;
         }
 
     }
