5afa77ea65ff55dd0d5b43e9e336604a3026dc18
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-18 10:36:59 +0100

    Removed old commented lines of code

--- Housefire/GXPEngine/GXPEngine/Objects/Handlers/Data/CheckingData.cs
+++ Housefire/GXPEngine/GXPEngine/Objects/Handlers/Data/CheckingData.cs
@@ -14,9 +14,6 @@ namespace GXPEngine.Objects.Handlers.Data
 
         public CheckingData(int noteTime, int hitTime, BeatmapHandler beatmapHandler)
         {
-            //hitTime -= (int)(beatmapHandler.BPM_calc / 4) - (int)(beatmapHandler.activeBeatmap.offset/4);
-            
-            //hitTime -= (int)(1000 - beatmapHandler.BPM_calc);
             noteTime -= (int)(1000 - beatmapHandler.BPM_calc);
 
             noteTime += BeatmapHandler.beatOffset;
