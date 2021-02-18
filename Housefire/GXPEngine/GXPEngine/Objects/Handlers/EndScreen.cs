6aed6b4a18cf536d73af2e075ba2ecd1ad69aa34
Glyceri <78602618+Glyceri@users.noreply.github.com>
2021-02-18 12:56:56 +0100

    End screen

---
+++ Housefire/GXPEngine/GXPEngine/Objects/Handlers/EndScreen.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine.Objects.Handlers
+{
+    public class EndScreen : GameObject
+    {
+        bool _canBeInteractedWith;
+        public bool canBeInteractedWith { get => _canBeInteractedWith && visible; set { canBeInteractedWith = value; } }
+
+
+        public EasyDraw backdrop;
+
+        public EndScreen()
+        {
+            backdrop = new EasyDraw(1920, 1080, false);
+            backdrop.Clear(Color.Black);
+            AddChild(backdrop);
+        }
+
+
+        void Update()
+        {
+
+        }
+
+    }
+}
